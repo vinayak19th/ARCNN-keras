b60929cc27624a66437e3ab1b9ee879d025959a4
vinayak19th <vinayak1998th@gmail.com>
2021-02-17 16:17:46 +0530

    Fixed Names

--- model.py
+++ model.py
@@ -29,6 +29,6 @@ def get_ARCNN_lite(input_shape=(32,32,1)):
     conv3 = Conv2D(32,5,dilation_rate=2,activation='relu', padding='same', use_bias=True,name="Feature_Enhance")(conv2)
     conv4 = Conv2D(32,1,activation='relu', padding='valid', use_bias=True,name="Mapping")(conv3)
     conv_trans = Conv2DTranspose(1,3,dilation_rate=4,name="Upscale",padding='same')(conv4)
-    ARCNN = Model(inputs=inp,outputs=conv_trans)
+    ARCNN = Model(inputs=inp,outputs=conv_trans,name="ARCNN_lite")
     return ARCNN
 
